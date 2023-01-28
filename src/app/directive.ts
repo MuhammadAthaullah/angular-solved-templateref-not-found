c889fe10f24103b5ed8cd6ac703c0ae38766d2a6
MuhammadAthaullah <101212414+MuhammadAthaullah@users.noreply.github.com>
2023-01-28 10:19:06 +0530

    1 file modified

--- src/app/directive.ts
+++ src/app/directive.ts
@@ -12,7 +12,8 @@ import {
   standalone: true,
 })
 export class MyIfDirective {
-  constructor(private tpl: TemplateRef<any>, private vcr: ViewContainerRef) {}
+  tpl: TemplateRef<any>
+  constructor( private vcr: ViewContainerRef) {}
 
   ngOnInit() {
     this.vcr.createEmbeddedView(this.tpl);
